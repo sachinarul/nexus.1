bb323dfb66f16408edd31d4384d2b6b86821fdf4
surjeet112 <surjeet@noblenexus-ie.com>
2026-04-13 18:04:34 +0530

    pushing of noble nexus website

--- server/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitContact, getAllContacts } = require('../controllers/contactController');
-
-router.post('/', submitContact);
-router.get('/', getAllContacts); // Should be protected for admin
-
-module.exports = router;
--- server/routes/feedbackRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createFeedback, getFeedbacks } = require('../controllers/feedbackController');
-
-router.post('/', createFeedback);
-router.get('/', getFeedbacks); // Should be protected in real app
-
-module.exports = router;
